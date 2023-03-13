c3dcb161968935eb619b30d797c3f1754eb0693f
Fernando Bártoli <54591089+fdbartoli@users.noreply.github.com>
2023-03-12 22:06:15 -0300

    To find the volume (centimeters cubed) of a cuboid you use the formula:
    volume = Length * Width * Height
    
    But someone forgot to use proper record keeping, so we only have the volume, and the length of a single side!
    
    It's up to you to find out whether the cuboid has equal sides (= is a cube).
    
    Return true if the cuboid could have equal sides, return false otherwise.
    
    Return false for invalid numbers too (e.g volume or side is less than or equal to 0).

---
+++ 01-CubeChecker/01-CubeChecker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_CubeChecker
+{
+    public class CubeChecker
+    {
+        public bool IsCube(double volume, double side)
+        {
+            
+            return volume > 0 && volume == Math.Pow(side, 3); //literal boolean remove (? true : false)
+        }
+    }
+}
